8f59637acadbcd9db99d1340fb313479a5a9352a
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2021-02-08 19:06:26 +0200

    second commit

--- src/js/task3.js
+++ src/js/task3.js
@@ -3,10 +3,12 @@ const randomIntegerFromInterval = (min, max) => {
   };
   
   const makeTransaction = transaction => {
-    const delay = randomIntegerFromInterval(200, 500);
+      
   
     return new Promise((resolve, reject) => {
-    setTimeout(() => {
+      const delay = randomIntegerFromInterval(200, 500);
+    
+       setTimeout(() => {
         const canProcess = Math.random() > 0.3;
     
         if (canProcess) {
@@ -14,29 +16,20 @@ const randomIntegerFromInterval = (min, max) => {
         } else {
           reject(transaction.id);
         }
-      }, delay);
+      },delay);
   })
     
   };
   
-  const logSuccess = (id, time) => {
-    console.log(`Transaction ${id} processed in ${time}ms`);
+  const logSuccess = arr => {
+    console.log(`Transaction ${arr[0]} processed in ${arr[1]}ms`);
   };
   
   const logError = id => {
     console.warn(`Error processing transaction ${id}. Please try again later.`);
   };
   
-  /*
-   * Работает так
-   */
-  //makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-  //makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-  //makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-  //makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-  /*
-   * Должно работать так
-   */
+  
   makeTransaction({ id: 70, amount: 150 })
     .then(logSuccess)
     .catch(logError);
